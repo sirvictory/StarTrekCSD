9ac46c8b220885eb71db39bd53f5c515a83decc6
shahidgill <shahidgill@SHAHIDGILL6948>
2017-10-05 11:28:43 -0500

    This includes changes to the ship class and damages and health of subsystems engine and phaser.

--- StarTrekNextGeneration/Ship.cs
+++ StarTrekNextGeneration/Ship.cs
@@ -1,14 +1,79 @@
-﻿using StarTrekNextGeneration;
+﻿using System;
+using StarTrekNextGeneration;
 
 namespace StarTrekNextGeneration
 {
     public class Ship
     {
         Shield shield;
+        public bool isEngineUsable { get; internal set; }
+        public bool isPhaserUsable { get; internal set; }
+
+        public int shipEnergy { get; internal set; }
+        public int shipWeaponsPhaser { get; internal set; }
+        public int shipWeaponsPhoton { get; internal set; }
+        public int shipEngines { get; internal set; }
+
 
         public Ship()
         {
             shield = new Shield();
+            
+            
         }
+
+        public void healEngine(int engineHealth)
+        {
+            shipEngines = engineHealth;
+            if(shipEngines > 100)
+            {
+                shipEngines = 100;
+            }
+
+
+            isEngineUsable = true;
+        }
+
+        public void damageEngine(int engineDamage)
+        {
+            shipEngines -= engineDamage;
+            if(shipEngines <= 0)
+            {
+                shipEngines = 0;
+                isEngineUsable = false;
+            }
+            else
+            {
+                isEngineUsable = true;
+            }
+        }
+
+        public void healPhaser(int phaserHealth)
+        {
+            shipWeaponsPhaser = phaserHealth;
+            if (phaserHealth > 100)
+            {
+                phaserHealth = 100;
+            }
+
+
+            isPhaserUsable = true;
+        }
+
+        public void damagePhaser(int phaserDamage)
+        {
+            shipWeaponsPhaser -= phaserDamage;
+            if (shipWeaponsPhaser <= 0)
+            {
+                shipWeaponsPhaser = 0;
+                isPhaserUsable = false;
+            }
+            else
+            {
+                isPhaserUsable = true;
+            }
+        }
+
+
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -90,5 +90,125 @@ namespace Tests
             //Then
             Assert.AreEqual(0, shield.Energy);
         }
+
+        [TestMethod]
+        public void HealEngine()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healEngine(100);
+            //Then
+            Assert.AreEqual(100, ship.shipEngines);
+
+        }
+
+        [TestMethod]
+        public void DamageEngine()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healEngine(100);
+            //When
+            ship.damageEngine(40);
+            //Then
+            Assert.AreEqual(60, ship.shipEngines);
+        }
+
+        [TestMethod]
+        public void EngineDamageNegative()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healEngine(100);
+            //When
+            ship.damageEngine(110);
+            //Then
+            Assert.AreEqual(0, ship.shipEngines);
+        }
+
+        [TestMethod]
+        public void IsEngineUsable()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healEngine(60);
+            //When
+            ship.damageEngine(100);
+            //Then
+            Assert.IsFalse(ship.isEngineUsable);
+        }
+
+        [TestMethod]
+        public void IsEngineUsableAfterHeal()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healEngine(60);
+            //When
+            ship.damageEngine(100);
+            ship.healEngine(10);
+            //Then
+            Assert.IsTrue(ship.isEngineUsable);
+        }
+
+        [TestMethod]
+        public void EngineMaxHealthCheck()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healEngine(60);
+            //When
+            ship.damageEngine(100);
+            ship.healEngine(200);
+            //Then
+            Assert.AreEqual(100, ship.shipEngines);
+        }
+
+        [TestMethod]
+        public void HealPhaser()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healPhaser(100);
+            //Then
+            Assert.AreEqual(100, ship.shipWeaponsPhaser);
+
+        }
+
+        [TestMethod]
+        public void PhaserDamageNegative()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healPhaser(100);
+            ship.damagePhaser(200);
+            //Then
+            Assert.AreEqual(0, ship.shipWeaponsPhaser);
+        }
+
+        [TestMethod]
+        public void IsPhaserUsable()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healPhaser(100);
+            ship.damagePhaser(200);
+            //Then
+            Assert.IsFalse(ship.isPhaserUsable);
+        }
+
+        [TestMethod]
+        public void IsPhaserUsableAfterHeal()
+        {
+            Ship ship = new Ship();
+            //Given
+            ship.healPhaser(100);
+            ship.damagePhaser(200);
+            ship.healPhaser(10);
+            //Then
+            Assert.IsTrue(ship.isPhaserUsable);
+        }
+
+
     }
 }
