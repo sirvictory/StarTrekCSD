8e57357e26315ad7fb80e14780421084a2120ae5
SrBilyon Harris <srbharris@gmail.com>
2017-10-05 11:50:37 -0500

    Revert "Merge of A2b"
    This reverts commit 05477811235c1434d1ddc45a8ddadcfa1bd0d76f.

--- StarTrekNextGeneration/Ship.cs
+++ StarTrekNextGeneration/Ship.cs
@@ -1,81 +1,24 @@
-﻿using System;
-using StarTrekNextGeneration;
+﻿using StarTrekNextGeneration;
 
 namespace StarTrekNextGeneration
 {
     public class Ship
     {
-        public struct Position
-        {
-            public int x;
-            public int y;
-        }
-
+        public Shield shield;
         public Phaser phaser;
         public Position position;
-        Shield shield;
-        public bool isEngineUsable { get; internal set; }
-        public bool isPhaserUsable { get; internal set; }
-
-        public int shipEnergy { get; internal set; }
-        public int shipWeaponsPhaser { get; internal set; }
-        public int shipWeaponsPhoton { get; internal set; }
-        public int shipEngines { get; internal set; }
 
         public Ship()
         {
             shield = new Shield();
             phaser = new Phaser();
         }
-
-        public void healEngine(int engineHealth)
-        {
-            shipEngines = engineHealth;
-            if (shipEngines > 100)
-            {
-                shipEngines = 100;
-            }
-            isEngineUsable = true;
-        }
-
-        public void damageEngine(int engineDamage)
-        {
-            shipEngines -= engineDamage;
-            if (shipEngines <= 0)
-            {
-                shipEngines = 0;
-                isEngineUsable = false;
-            }
-            else
-            {
-                isEngineUsable = true;
-            }
-        }
-
-        public void healPhaser(int phaserHealth)
-        {
-            shipWeaponsPhaser = phaserHealth;
-            if (phaserHealth > 100)
-            {
-                phaserHealth = 100;
-            }
-
-
-            isPhaserUsable = true;
-        }
-
-        public void damagePhaser(int phaserDamage)
-        {
-            shipWeaponsPhaser -= phaserDamage;
-            if (shipWeaponsPhaser <= 0)
-            {
-                shipWeaponsPhaser = 0;
-                isPhaserUsable = false;
-            }
-            else
-            {
-                isPhaserUsable = true;
-            }
-        }
     }
+
+    public struct Position
+    {
+        public int x;
+        public int y;
+    }
+
 }
