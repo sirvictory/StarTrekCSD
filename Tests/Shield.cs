7033f6c68d1878b8179752c149a8c2b83fc64152
SrBilyon Harris <srbharris@gmail.com>
2017-10-05 09:28:54 -0500

    Initial Commit

---
+++ Tests/Shield.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StarTrekNextGeneration
+{
+    internal class Shield
+    {
+        public bool IsUp { get; internal set; }
+        public int Energy { get; internal set; }
+        public const int StartingEnergy = 10000;
+         
+        public Shield()
+        {
+            IsUp = false;
+            Energy = StartingEnergy;
+        }
+
+        internal void Raise()
+        {
+            IsUp = true;
+        }
+
+        internal void TakeHit(int v)
+        {
+            Energy -= v;
+            if (Energy < 0)
+            {
+                Energy = 0;
+            }
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StarTrekNextGeneration;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Shield shield;
+
+        [TestInitialize]
+        public void MakeNewShield()
+        {
+            shield = new Shield();
+        }
+
+        [TestMethod]
+        public void ShieldsDownByDefault()
+        {
+            Assert.IsFalse(shield.IsUp);
+        }
+
+        [TestMethod]
+        public void ShieldsAreUpAfterWeRaiseThem()
+        {
+            shield.Raise();
+            Assert.IsTrue(shield.IsUp);
+        }
+
+        [TestMethod]
+        public void ShieldChargedByDefault()
+        {
+            Assert.AreEqual(Shield.StartingEnergy, shield.Energy);
+        }
+
+        [TestMethod]
+        public void ShieldStartingEnergyIsTenThousand()
+        {
+            Assert.AreEqual(10000, Shield.StartingEnergy);
+        }
+
+        [TestMethod]
+        public void ShieldIsAtMinimum()
+        {
+            //Given
+            shield.Energy = 0;
+            //When
+            shield.TakeHit(10);
+            //Then
+            Assert.AreEqual(0, shield.Energy);
+        }
+
+        [TestMethod]
+        public void ReducesToMinimum()
+        {
+            //Given
+            shield.Energy = 9;
+            //When
+            shield.TakeHit(10);
+            //Then
+            Assert.AreEqual(0, shield.Energy);
+        }
+
+        [TestMethod]
+        public void ShieldReductionIsCorrect()
+        {
+            //Given
+            shield.Energy = 21;
+            //When
+            shield.TakeHit(10);
+            //Then
+            Assert.AreEqual(11, shield.Energy);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void DamageIsNegative()
+        {
+            shield.TakeHit(-10);   
+        }
+
+
+    }
+}
